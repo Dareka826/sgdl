d919f9c2913246441b8f98a4991b8f877b8554f9
Rin Takanashi <dareka826@gmail.com>
2021-07-28 12:02:29 +0200

    main.c: fetch urls for ids in arguments

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "get_image.h"
 
 int main(int argc, char **argv) {
+
+	if(argc <= 1) {
+		printf("Usage: %s ids...\n", argv[0]);
+		return EXIT_SUCCESS;
+	}
+
 	char url[200];
-	sgdl_get_image(6311288, url, 200);
-	printf("RESULT: %s\n", url);
+	for(int i = 1; i < argc; i++) {
+		int id = atoi(argv[i]);
+		if(sgdl_get_image(id, url, 200) != 0)
+			strncpy(url, "ERROR", 6);
+		printf("[%d]: %s\n", id, url);
+	}
 
 	return EXIT_SUCCESS;
 }
