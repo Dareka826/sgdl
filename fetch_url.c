74f4600cacf02da10234a3c4bef29ef5f50dd7ce
Rin Takanashi <dareka826@gmail.com>
2021-08-04 11:55:09 +0200

    add error codes to fetch_url remove unnecessary includes

--- fetch_url.c
+++ fetch_url.c
@@ -1,22 +1,19 @@
 #include "fetch_url.h"
 
 #include <curl/curl.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
-#include <assert.h>
 
-int init_fetch_url_result(struct fetch_url_result *r);
-int destroy_fetch_url_result(struct fetch_url_result *r);
+enum fetch_url_code init_fetch_url_result(struct fetch_url_result *r);
+enum fetch_url_code destroy_fetch_url_result(struct fetch_url_result *r);
 
 size_t fetch_url_curl_writefunc(void *ptr, size_t size, size_t nmemb,
 		struct fetch_url_result *r);
 
-int fetch_url(char *url, struct fetch_url_result *result) {
-	int ret = 0;
+enum fetch_url_code fetch_url(char *url, struct fetch_url_result *result) {
+	enum fetch_url_code ret = OK;
 
 	CURL *curl = curl_easy_init();
-	if(!curl) return 1;
+	if(!curl) return CURL_ERR;
 
 	curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1);
 	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
@@ -26,30 +23,30 @@ int fetch_url(char *url, struct fetch_url_result *result) {
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, result);
 
 	if(curl_easy_perform(curl) != CURLE_OK)
-		ret = 1;
+		ret = CURL_ERR;
 
 	curl_easy_cleanup(curl);
 
 	return ret;
 }
 
-int init_fetch_url_result(struct fetch_url_result *r) {
+enum fetch_url_code init_fetch_url_result(struct fetch_url_result *r) {
 	r->ptr = (char*) malloc(1);
-	if(r->ptr == NULL) return 1;
+	if(r->ptr == NULL) return MALLOC_ERR;
 
 	r->len = 0;
 	r->ptr[0] = '\0';
 
-	return 0;
+	return OK;
 }
 
-int destroy_fetch_url_result(struct fetch_url_result *r) {
-	if(r->ptr == NULL) return 1;
+enum fetch_url_code destroy_fetch_url_result(struct fetch_url_result *r) {
+	if(r->ptr == NULL) return NULLPTR_ERR;
 
 	free(r->ptr);
 	r->len = -1;
 
-	return 0;
+	return OK;
 }
 
 size_t fetch_url_curl_writefunc(void *ptr, size_t size, size_t nmemb,
@@ -59,7 +56,7 @@ size_t fetch_url_curl_writefunc(void *ptr, size_t size, size_t nmemb,
 
 	size_t new_len = r->len + data_size;
 	r->ptr = realloc(r->ptr, new_len+1);
-	if(r->ptr == NULL) return -1;
+	if(r->ptr == NULL) return 0; // Notify curl that no data was processed
 
 	memcpy(r->ptr + r->len, ptr, data_size);
 
--- fetch_url.h
+++ fetch_url.h
@@ -3,14 +3,22 @@
 
 #include <stdlib.h>
 
+enum fetch_url_code {
+	OK,
+	CURL_ERR,
+	MALLOC_ERR,
+	NULLPTR_ERR,
+
+};
+
 struct fetch_url_result {
 	char *ptr;
 	size_t len;
 };
 
-int fetch_url(char *url, struct fetch_url_result *result);
+enum fetch_url_code fetch_url(char *url, struct fetch_url_result *result);
 
-int init_fetch_url_result(struct fetch_url_result *r);
-int destroy_fetch_url_result(struct fetch_url_result *r);
+enum fetch_url_code init_fetch_url_result(struct fetch_url_result *r);
+enum fetch_url_code destroy_fetch_url_result(struct fetch_url_result *r);
 
 #endif
